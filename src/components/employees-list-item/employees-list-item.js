f03c3f76061405a80e679c934501925edefba710
Schislyaev <80824032+Schislyaev@users.noreply.github.com>
2022-05-19 14:27:24 +0300

    Added forms

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,24 +1,33 @@
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => {
+const EmployeesListItem  = (props) => {
+
+    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
     let classNameInc = "list-group-item d-flex justify-content-between"
     if (increase) {
-        classNameInc = "list-group-item d-flex justify-content-between increase"
+        classNameInc += " increase";
     }
+    if (rise) {
+        classNameInc += " like"
+    }
+    
 
     return (
         <li className={classNameInc}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm "
+                    onClick={onToggleProp}
+                    data-toggle="increase">
                     <i className="fas fa-cookie"></i>
                 </button>
 
                 <button type="button"
-                        className="btn-trash btn-sm ">
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
                 <i className="fas fa-star"></i>
